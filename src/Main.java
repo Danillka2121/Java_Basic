2e58b3f6bae5d0bb7f6a35a80dcd4f5b8bb0b698
Danillka2121 <tarkovskijdanil9@gmail.com>
2025-01-27 18:06:59 +0200

    Lab_0

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/*
+Лабораторна робота номер 0. Hello world
+Встановити Intelliji Idea та Java jdk останньої версії.
+Cтворити maven проект та розробити в цьому проекті типову програму Hello world.
+Програма повинна видавати на екран напис Hello world та закінчувати свою роботу.
+*/
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!!!");
+    }
+}
